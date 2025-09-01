3b22cdd16dd9ac60994e5e2307c96db788cda009
Masoud Andiwal <Masoudandiwal89@gmail.com>
2025-09-01 16:28:56 +0430

    Add dashboard header and total balance components
    Introduced HeaderBox, TotalBalanceBox, and AnimatedCounter components for the home page. Updated the home page layout to include a personalized greeting and total balance display. Enhanced global styles for improved alignment and responsiveness. Added 'react-countup' dependency for animated balance display.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
-function page() {
+function Home() {
+  const loginIn = {firstName : "Masoud"}
   return (
-    <div>page</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                type="greeting"
+                title="welcome"
+                user={loginIn?.firstName || "Guest"}
+                subtext="Access and manage you account and transactions efficiently"
+                />
+
+
+                <TotalBalanceBox 
+                  accounts={[]}
+                  totalBanks={1}
+                  totalCurrentBalance={1250.35}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
-export default page
+export default Home
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,14 @@
+ 'use client'
+import React from 'react'
+import CountUp from 'react-countup'
+function AnimatedCounter({amount}:{amount:number}) {
+  return (
+    <span>
+        <CountUp 
+        decimal=','
+        prefix='$ '
+        end={amount}/>
+    </span>
+  )
+}
+export default AnimatedCounter
